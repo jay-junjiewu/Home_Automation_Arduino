3061a3912c855aff1ef429ebb0c5662107f804f3
Jay <142403088+jay-junjiewu@users.noreply.github.com>
2023-08-17 22:21:16 +1000

    Update MEEBO_Arduino.ino

--- MEEBO_Arduino.ino
+++ MEEBO_Arduino.ino
@@ -1,22 +1,3 @@
-/***********************************************************************************
-*This program is an demo autonomous home system called "MEEBO"
-*This demo was made for LCD modules with 8bit data port
-*This program requires the LCDKIWI and Touchscreen library.
-
-* File                : MEEBO_Arduino
-* Hardware Environment: Arduino UNO&Mega2560
-* Build Environment   : Arduino
-
-*Set the pins to the correct ones for your development shield or breakout board.
-*                  LCD_CS  LCD_CD  LCD_WR  LCD_RD  LCD_RST  SD_SS  SD_DI  SD_DO  SD_SCK 
-*     Arduino Uno    A3      A2      A1      A0      A4      10     11     12      13                            
-*Arduino Mega2560    A3      A2      A1      A0      A4      10     11     12      13                           
-
-*                  LCD_D0  LCD_D1  LCD_D2  LCD_D3  LCD_D4  LCD_D5  LCD_D6  LCD_D7  
-*     Arduino Uno    8       9       2       3       4       5       6       7
-*Arduino Mega2560    8       9       2       3       4       5       6       7 
-*
-**********************************************************************************/
 #include <TouchScreen.h> // Touch library
 #include <LCDWIKI_GUI.h> // Core graphics library
 #include <LCDWIKI_KBV.h> // Hardware-specific library
